78adf12224c69f1d2a18b498775508344d25973f
Rixongotelo Nathaniel Baloyi <rixobaloyi@gmail.com>
2026-02-22 18:00:32 +0200

    updated the tragger

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/thulanyathi-built-strong-main/", // 👈 ADD THIS LINE
-
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-
+export default defineConfig({
+  base: "/thulanyathi-built-strong-main/", // 👈 important
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: { overlay: false },
+  },
+});
